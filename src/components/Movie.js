3971f5331172cc81628a16ab205b08533fbbce3c
gangslee <grandnet1225@gmail.com>
2020-11-22 00:18:48 +0900

    Local State part Two

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,14 @@
+import { useApolloClient, useMutation } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+const LIKE_MOVIE = gql`
+  mutation likeMovie($id: Int!) {
+    likeMovie(id: $id) @client
+  }
+`;
 
 const Container = styled.div`
   height: 340px;
@@ -18,11 +26,15 @@ const Poster = styled.div`
   background-position: center center;
 `;
 
-export default ({ id, bg, isLiked }) => (
-  <Container>
-    <Link to={`/${id}`}>
-      <Poster bg={bg} />
-    </Link>
-    <button>{isLiked ? 'Unlike' : 'Like'}</button>
-  </Container>
-);
+export default ({ id, bg, isLiked }) => {
+  const [likeMovie] = useMutation(LIKE_MOVIE, { variables: { id: parseInt(id) } });
+
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={isLiked ? null : likeMovie}>{isLiked ? 'Unlike' : 'Like'}</button>
+    </Container>
+  );
+};
